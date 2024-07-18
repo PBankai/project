88b5c371dbca25a91adc79bc02ab524b24895a37
pedro alberto estrada hernandez <pedro.eshez@gmail.com>
2024-07-17 18:15:59 -0600

    initial commit

---
+++ src/test/java/com/junitworkshop/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junitworkshop.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
